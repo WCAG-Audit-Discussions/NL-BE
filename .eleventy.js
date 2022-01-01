72c71547ad7f9b9a3c3689df1ca0054056c993d8
rianrietveld <rian@level-level.com>
2022-01-01 21:18:08 +0100

    setup in eleverty

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,27 @@
+const slugify = require("slugify");
+
 module.exports = function(eleventyConfig) {
+
+	eleventyConfig.addPassthroughCopy('./src/css');
+
+	// Universal slug filter strips unsafe chars from URLs
+	eleventyConfig.addFilter("slugify", function (str) {
+		return slugify(str.replace(/<\/?("[^"]*"|'[^']*'|[^>])*(>|$)/g, ""), {
+			lower: true,
+			replacement: "-",
+			remove: /[*+~.·,()'"`´%!?¿:@»]/g,
+		});
+	});
+
 	// Return your Object options:
 	return {
-		dir: {
-			input: "src",
-			output: "docs"
-		}
+		"passthroughFileCopy": true,
+		"dir": {
+			"input": "src",
+			"includes": "_includes",
+			"data": "_data",
+			"output": "docs",
+		},
 	}
 };
 
